9d045b875dba960b80a5fcd82d2e3338ad540ae4
Srimadura <srimadura.l.2024.it@rajalakshmi.edu.in>
2026-06-15 21:53:33 +0530

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,1 +1,15 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
+data = pd.read_csv("dataset.csv")
+
+X = data[["Attendance","StudyHours","PreviousMarks"]]
+y = data["Performance"]
+
+model = RandomForestClassifier()
+
+model.fit(X,y)
+
+prediction = model.predict([[85,4,75]])
+
+print("Predicted Performance:",prediction[0])
